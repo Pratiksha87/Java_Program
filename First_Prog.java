6fe97084f46ab20993f4ac16c584cda194771ee9
Pratiksha87 <60502153+Pratiksha87@users.noreply.github.com>
2020-06-30 13:30:42 +0530

    First java program on android studio.

---
+++ First_Prog.java
@@ -0,0 +1,13 @@
+package pratiksha.garkar.pratiksha;
+
+import java.util.Scanner;
+
+public class First_Prog {
+    public static void main(String[] args) {
+        float p=2,r=2,t=2,SI=0;
+        SI=p*r*t;
+        SI=SI/100;
+        System.out.println("SI "+SI);
+
+    }
+}
