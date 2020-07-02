5d41c2f38b8582df11a248148a8ea0b7e455ac16
Pratiksha87 <60502153+Pratiksha87@users.noreply.github.com>
2020-07-02 23:18:25 +0530

    How boolean type variable work with boolean logical operators.

---
+++ Boolean_Prog.java
@@ -0,0 +1,18 @@
+package pratiksha.garkar.pratiksha;
+
+public class Boolean_Prog {
+    public static void main(String[] args) {
+        System.out.println("Boolean");
+
+// these are boolean variables
+        boolean A = true;
+
+        boolean B = false;
+
+        System.out.println("A|B = " + (A | B));
+        System.out.println("A&B = " + (A & B)); // program execution using boolean
+        System.out.println("!A = " + (!A));
+        System.out.println("A^B = " + (A ^ B));
+        System.out.println("(A|B)&A = " + ((A | B) & A));
+    }
+}
